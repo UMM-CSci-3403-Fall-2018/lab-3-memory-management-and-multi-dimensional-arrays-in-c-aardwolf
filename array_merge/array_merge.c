a7b8909eb6a55da2a73acb9efb3bea8cf27265fe
SirLich <lrkoehler77@gmail.com>
2018-09-27 11:17:43 -0500

    Added psudo-code solution for reference

---
+++ array_merge/array_merge.c
@@ -0,0 +1,9 @@
+#include "array_merge.h"
+
+int* array_merge(int num_arrays, int* sizes, int** values){
+  // set returnThis;
+  // for array in arrayList:
+  //   for element in array:
+  //     set.add(element)
+  //   return set.asArray;
+}
