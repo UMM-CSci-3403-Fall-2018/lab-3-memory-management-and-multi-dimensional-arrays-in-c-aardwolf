a446e104f8402a08abf8738e6ad0f8f55bc879c8
SirLich <lrkoehler77@gmail.com>
2018-09-27 11:23:30 -0500

    added helper code

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -6,4 +6,30 @@ int* array_merge(int num_arrays, int* sizes, int** values){
   //   for element in array:
   //     set.add(element)
   //   return set.asArray;
+
+
+// int test[3][2] = {{1,4},{2,5},{2,8}};
+//
+// // Define a pointer to walk the rows of the 2D array.
+// int (*p1)[2] = test;
+//
+// // Define a pointer to walk the columns of each row of the 2D array.
+// int *p2 = NULL;
+//
+// // There are three rows in the 2D array.
+// // p1 has been initialized to point to the first row of the 2D array.
+// // Make sure the iteration stops after the third row of the 2D array.
+// for (; p1 != test+3; ++p1) {
+//
+//     // Iterate over each column of the arrays.
+//     // p2 is initialized to *p1, which points to the first column.
+//     // Iteration must stop after two columns. Hence, the breaking
+//     // condition of the loop is when p2 == *p1+2
+//     for (p2 = *p1; p2 != *p1+2; ++p2 ) {
+//         printf("%d\n", *p2);
+//     }
+// }
+
+
+
 }
