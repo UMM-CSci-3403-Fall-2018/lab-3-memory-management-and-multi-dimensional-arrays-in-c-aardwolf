78cd8383c0494ff7e5529a0eedfb62ec2700b5a9
yoaku005 <yoaku005@d.umn.edu>
2018-10-02 11:06:25 -0500

    fixed error for sorted lists

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -3,12 +3,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//int main(int argc, char *argv[]){
+  // int a[] = {1,2,3,4,5};
+  // mergesort(5, a);
+  //print_array(5,a);
+// }
+//
+// void print_array(int length, int *array)
+// {
+//     for (int i = 0; i < length; i++) {
+//         printf("%d, ",array[i]);
+//     }
+// }
 
 bool needSorting(int rangeSize){
   return rangeSize >= 2;
 }
-
-
 
 void mergeRanges(int values[], int start, int mid, int end){
   int rangeSize = end- start;
@@ -20,7 +30,7 @@ void mergeRanges(int values[], int start, int mid, int end){
 
   while (firstIndex < mid && secondIndex < end){
     if(values[firstIndex]< values[secondIndex]){
-      destination[copyIndex]= values[secondIndex];
+      destination[copyIndex]= values[firstIndex];
       ++firstIndex;
     } else {
       destination[copyIndex]=values[secondIndex];
@@ -46,8 +56,12 @@ void mergeRanges(int values[], int start, int mid, int end){
   }
   free(destination);
 }
+
 void mergesortRange(int values[], int start, int end){
   int rangeSize = end - start;
+  //print_array(rangeSize, values);
+  //printf("\n");
+
   if(needSorting(rangeSize)){
     int midPoint = (start + end) / 2;
     mergesortRange(values, start, midPoint);
@@ -56,8 +70,6 @@ void mergesortRange(int values[], int start, int end){
   }
 }
 
-
 void mergesort(int size, int values[]){
 	mergesortRange(values,0,size);
 }
-
