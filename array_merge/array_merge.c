cfc20dfdd92b3e61cd609d337b223701273204b2
SirLich <lrkoehler77@gmail.com>
2018-10-02 10:33:21 -0500

    Added prints for debugging array states during array_merge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -10,21 +10,29 @@ int main(void)
 
   //Test data pulled from array_merge_test.cpp
   int num_arrays = 10;
-  int sizes[] = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+  int sizes[] = { 10, 10, 10, 10, 10, 10, 10, 10, 10 };
   int a0[] = { 3, 2, 0, 5, 8, 9, 6, 3, 2, 0 };
-  int* a[] = { a0, a0, a0, a0, a0, a0, a0, a0, a0, a0 };
-  int expected[] = { 7, 0, 2, 3, 5, 6, 8, 9 };
+  int a1[] = { 5, 8, 9, 3, 2, 0, 6, 3, 2, 0 };
+  int a2[] = { 8, 5, 0, 2, 3, 0, 2, 3, 6, 9 };
+  int* a[] = { a0, a1, a2, a0, a1, a2, a0, a1, a2 };
+
   int* result;
 
   result = array_merge(num_arrays, sizes, a);
+  int count = result[0];
+
+
+  printf("Output:");
+  print_array(count, result);
   return 0;
 }
 
 //Helper function to pritn arrays
-void print_array(int* array){
-  for (i = 0; i < 10; i += 2){
+void print_array(int size, int* array){
+  for (int i = 0; i < size; i += 2){
     printf( "%d, ", array[i]);
   }
+  printf("\n");
 }
 
 int* array_merge(int num_arrays, int* sizes, int** values){
@@ -36,19 +44,46 @@ int* array_merge(int num_arrays, int* sizes, int** values){
   }
 
   //Create results based on count
-  int* results = malloc(count*sizeof(int));
+  int* results = malloc((count*sizeof(int))+1);
 
   //Position is the current append position in results
-  int position = 0;
+  int position = 1;
+
+  //Count as the first position -Remember that we still need to optimize this
+  results[0] = count;
 
   //Loop over each sub-array
   for(int i = 0; i < num_arrays; i++){
 
+    //print
+    print_array(sizes[i],values[i]);
+
     //Loop over each element of each sub-array
     for(int j = 0; j < sizes[i]; j++){
       results[position] = values[i][j];
+      printf(values[i][j] + "\n");
       position++;
     }
   }
   return sizes;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Comment
