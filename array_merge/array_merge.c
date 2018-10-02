80c9f82008c22921eb0ce900b3d2c9701837844d
SirLich <lrkoehler77@gmail.com>
2018-10-01 20:16:31 -0500

    Added skeleton-code implementation.
    Still filled with compiliation errors, space-alocation issues, and memory leaks

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,35 +1,35 @@
+#include<stdio.h>
 #include "array_merge.h"
 
+int main(void)
+{
+  printf("Program started!\n");
+
+  int num_arrays = 10;
+  int sizes[] = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+  int a0[] = { 3, 2, 0, 5, 8, 9, 6, 3, 2, 0 };
+  int* a[] = { a0, a0, a0, a0, a0, a0, a0, a0, a0, a0 };
+  int expected[] = { 7, 0, 2, 3, 5, 6, 8, 9 };
+  int* result;
+
+  result = array_merge(num_arrays, sizes, a);
+  return 0;
+}
+
 int* array_merge(int num_arrays, int* sizes, int** values){
-  // set returnThis;
-  // for array in arrayList:
-  //   for element in array:
-  //     set.add(element)
-  //   return set.asArray;
+  int count;
+  for(i = 0; i < num_arrays; i ++){
+    count += sizes[i]
+  }
 
+  int* results = malloc(count*sizeof(int));
+  int position = 0;
 
-// int test[3][2] = {{1,4},{2,5},{2,8}};
-//
-// // Define a pointer to walk the rows of the 2D array.
-// int (*p1)[2] = test;
-//
-// // Define a pointer to walk the columns of each row of the 2D array.
-// int *p2 = NULL;
-//
-// // There are three rows in the 2D array.
-// // p1 has been initialized to point to the first row of the 2D array.
-// // Make sure the iteration stops after the third row of the 2D array.
-// for (; p1 != test+3; ++p1) {
-//
-//     // Iterate over each column of the arrays.
-//     // p2 is initialized to *p1, which points to the first column.
-//     // Iteration must stop after two columns. Hence, the breaking
-//     // condition of the loop is when p2 == *p1+2
-//     for (p2 = *p1; p2 != *p1+2; ++p2 ) {
-//         printf("%d\n", *p2);
-//     }
-// }
-
-
-
+  for(i = 0; i < num_arrays; i++){
+    for(j = 0; j < sizes[i]){
+      results[position] = values[i][j];
+      position += 1;
+    }
+  }
+  return sizes;
 }
